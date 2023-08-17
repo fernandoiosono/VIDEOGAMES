c5f9da44933558fe4dc21951af1a4d463d59ebd1
fernandoiosono <fernandoiosono@gmail.com>
2023-08-17 10:33:32 -0600

    Server Raised & Connected to DB

--- Server/index.js
+++ Server/index.js
@@ -4,18 +4,18 @@ const morgan = require('morgan');
 const server = require('./src/server.js');
 const { database } = require('./src/database/database.js')
 
-const { handleUsers, 
-	handleFavorites, 
-	handleCharacters } = require('./src/routes/index.js');
+// const { handleUsers, 
+// 	handleFavorites, 
+// 	handleCharacters } = require('./src/routes/index.js');
 
 const { LOCALHOST_PORT } = process.env;
 
 server.use(cors());
 server.use(morgan("dev"));
 
-server.use('/rickandmorty/users', handleUsers);
-server.use('/rickandmorty/favorites', handleFavorites);
-server.use('/rickandmorty/characters', handleCharacters);
+// server.use('/rickandmorty/users', handleUsers);
+// server.use('/rickandmorty/favorites', handleFavorites);
+// server.use('/rickandmorty/characters', handleCharacters);
 
 database.sync({ force: true })
 	.then(() => {
