e79f4ef3512e71d1b88519bd441972d15f20aa36
fernandoiosono <fernandoiosono@gmail.com>
2023-08-19 10:39:16 -0600

    Now Suports Various Pages of API Genres (Also Implemented for Platforms

--- Server/src/controllers/Genres/getGenres.js
+++ Server/src/controllers/Genres/getGenres.js
@@ -5,19 +5,24 @@ const { Genres } = require('../../database/database.js');
 const { RAWG_URL_GENRES, RAWG_API_KEY } = process.env;
 
 const getGenres = async () => {
-    const genresDB = await Genres.findAll();
-    const { data } = await axios(`${RAWG_URL_GENRES}?key=${RAWG_API_KEY}`);
-
     const arrResult = [];
+    const genresDB = await Genres.findAll();
+    let nextPage = `${RAWG_URL_GENRES}?key=${RAWG_API_KEY}`;
 
-    for (let x = 0; x < data.results.length; x++) {
-        const genre = data.results[x];
-        const obj = { id: genre.id, name: genre.name };
-        
-        arrResult.push(obj);
-    }
+    do {
+        const { data } = await axios(nextPage);
 
-    arrResult.sort((a, b) => { return a.id - b.id });
+        for (let x = 0; x < data.results.length; x++) {
+            const genre = data.results[x];
+            const obj = { id: genre.id, name: genre.name };
+            
+            arrResult.push(obj);
+        }
+    
+        arrResult.sort((a, b) => { return a.id - b.id });
+
+        nextPage = data.next;
+    } while (nextPage);
 
     if (!genresDB.length) await Genres.bulkCreate(arrResult);
     
