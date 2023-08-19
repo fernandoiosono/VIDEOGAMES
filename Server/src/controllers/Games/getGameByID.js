e54c3e16882139f68723aac70854510b15b90bcc
fernandoiosono <fernandoiosono@gmail.com>
2023-08-19 11:36:06 -0600

    getGameByID Route & Controller Now Get Info from API

--- Server/src/controllers/Games/getGameByID.js
+++ Server/src/controllers/Games/getGameByID.js
@@ -4,10 +4,34 @@ const axios = require('axios');
 
 const { RAWG_URL_GAMES, RAWG_API_KEY } = process.env;
 
-const getVGByID = async (id) => {
-    const { data } = await axios(`${RAWG_URL_GAMES}/${id}?key=${RAWG_API_KEY}`);
+const getVGByID = async (pID) => {
+    const { data } = await axios(`${RAWG_URL_GAMES}/${pID}?key=${RAWG_API_KEY}`);
 
-    return data;
+    const { id, name, background_image, released, rating,
+        description, platforms, genres } = data;
+
+    const obj = { id, name, background_image, released, rating,
+        genres: getArrIDs(genres, 'genre'), 
+        platforms: getArrIDs(platforms, 'platform'), 
+        description };
+
+    return obj;
+};
+
+const getArrIDs = (obj, type) => { // Get an Ordered Array of ID Genres
+    const arrIDs = [];
+
+    for (let x = 0; x < obj.length; x++) {
+        const id = (type === 'genre') 
+            ? obj[x].id
+            : obj[x].platform.id;
+
+        arrIDs.push(id);
+    }
+
+    arrIDs.sort((a, b) => { return a - b });
+
+    return arrIDs;
 };
 
 module.exports = getVGByID;
---
+++ Server/src/routes/handlerGames.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+const errorHandler = require('../middlewares');
+
+const { getGameByID,
+    getGames,
+    getGamesByName,
+    postGame } = require('../controllers');
+
+router.get('/:id', errorHandler(async (req, res) => {
+    const { id } = req.params;
+    const game = await getGameByID(id);
+
+    res.status(200).json(game);
+}));
+
+// router.get('/all', errorHandler(async (req, res) => {
+//     const games = await getGames();
+
+//     res.status(200).json(games);
+// }));
+
+// router.get('/name', errorHandler(async (req, res) => {
+//     const { name } = req.query;
+//     const games = await getGamesByName(name);
+
+//     res.status(200).json(games);
+// }));
+
+// router.post('/', errorHandler(async (req, res) => {
+//     const { game } = req.body;
+//     const newGame = await postGame(game);
+
+//     res.status(200).json(newGame);
+// }));
+
+module.exports = router;
