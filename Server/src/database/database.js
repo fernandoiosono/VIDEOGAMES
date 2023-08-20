710cc502f7e1285d131a575a37d075540920c4ad
Fernando Venegas <fernandoiosono@gmail.com>
2023-08-20 13:32:25 -0600

    Relations Created

--- Server/src/database/database.js
+++ Server/src/database/database.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const { gameModel, genreModel, platformModel } = require('./models');
+
+const { defineGame, 
+    defineGenre, 
+    definePlatform } = require('./models');
 
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
@@ -8,13 +11,46 @@ const conn = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`;
 
 const database = new Sequelize(conn, { logging: false });
 
-// gameModel(database);
-genreModel(database);
-platformModel(database);
+defineGame(database);
+defineGenre(database);
+definePlatform(database);
 
-const { Genres } = database.models;
+const { Games, Genres, Platforms } = database.models;
+
+Games.belongsToMany(Genres, { 
+    through: "GamesGenres",
+    foreignKey: "idGame",
+    otherKey: "idGenre",
+    timestamps: false
+});
+
+Genres.belongsToMany(Games, { 
+    through: "GamesGenres",
+    foreignKey: "idGenre",
+    otherKey: "idGame",
+    timestamps: false
+});
+
+Games.belongsToMany(Platforms, { 
+    through: "GamesPlatforms",
+    foreignKey: "idGame",
+    otherKey: "idPlatform",
+    timestamps: false
+});
+
+Platforms.belongsToMany(Games, { 
+    through: "GamesPlatforms",
+    foreignKey: "idPlatform",
+    otherKey: "idGame",
+    timestamps: false
+});
 
 module.exports = {
     database,
     ...database.models
 };
+
+/*
+INSERT INTO "Games" (id, name, description, image, released, rating)
+VALUES ('b19d597c-8f54-41ba-ba73-02299c1adf92', 'Nombre del Juego', 'Descripción del Juego', 'ruta_de_la_imagen.jpg', now(), 4.5);
+*/
