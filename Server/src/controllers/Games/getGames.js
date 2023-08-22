626f6bbcfc6d6e1339b7ee4bdddafe78e162f7c2
fernandoiosono <fernandoiosono@gmail.com>
2023-08-22 10:35:50 -0600

    Better Games Count Functionality

--- Server/src/controllers/Games/getGames.js
+++ Server/src/controllers/Games/getGames.js
@@ -6,6 +6,7 @@ const { RAWG_URL_GAMES, RAWG_API_KEY } = process.env;
 let nextPage = `${RAWG_URL_GAMES}?key=${RAWG_API_KEY}`;
 
 const getGames = async () => {
+    let countGames = 100;
     const arrGames = [];
 
     // Get Games From DataBase @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
@@ -37,46 +38,46 @@ const getGames = async () => {
 
         arrGames.push(game);
     }
+
+    countGames -= dataGames.length;
     
     // Get Games From API @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     do {
         const { data } = await axios(nextPage);
 
         for (let x = 0; x < data.results.length; x++) {
-            const obj = data.results[x];
+            if (countGames) {
+                const obj = data.results[x];
             
-            const game = { id: obj.id, 
-                name: obj.name, 
-                image: obj.background_image,
-                rating: obj.rating,
-                genres: getArrNames(obj.genres, 'genre', 'api') };
+                const game = { id: obj.id, 
+                    name: obj.name, 
+                    image: obj.background_image,
+                    rating: obj.rating,
+                    genres: getArrNames(obj.genres) };
 
-            arrGames.push(game);
+                arrGames.push(game);
+                countGames -= 1;
+            } else {
+                return arrGames; // Si a mitad de una página ya se cumplió la meta de juegos, sale de la función
+            }
         }
 
         nextPage = data.next;
-    } while (arrGames.length < 21);
+    } while (arrGames.length < 100);
 
     return arrGames;
 };
 
-const getArrNames = (obj, type, origin) => { // Get an Ordered Array of ID Genres
-    const arrIDs = [];
+const getArrNames = (obj) => { // Get an Ordered Array of ID Genres
+    const arrNames = [];
 
     for (let x = 0; x < obj.length; x++) {
-        const name = 
-            (origin === "database")
-                ? (type === "genre")
-                    ? obj[x].dataValues.name
-                    : obj[x].dataValues.name
-                : (type === "genre")
-                    ? obj[x].name
-                    : obj[x].platform.name;
+        const name = obj[x].name;
 
-        arrIDs.push(name);
+        arrNames.push(name);
     }
 
-    return arrIDs;
+    return arrNames;
 };
 
 module.exports = getGames;
