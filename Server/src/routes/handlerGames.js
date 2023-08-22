9ed9e8faffcd0a713cb6002eaf9834e5ac4a2f77
fernandoiosono <fernandoiosono@gmail.com>
2023-08-22 09:31:43 -0600

    Now Handles getGamesByName Route

--- Server/src/routes/handlerGames.js
+++ Server/src/routes/handlerGames.js
@@ -12,19 +12,19 @@ router.get('/all', errorHandler(async (req, res) => {
     res.status(200).json(games);
 }));
 
+router.get('/name', errorHandler(async (req, res) => {
+    const { name } = req.query;
+    const games = await getGamesByName(name);
+
+    res.status(200).json(games);
+}));
+
 router.get('/:id', errorHandler(async (req, res) => {
     const { id } = req.params;
     const game = await getGameByID(id);
 
     res.status(200).json(game);
 }));
-
-// router.get('/name', errorHandler(async (req, res) => {
-//     const { name } = req.query;
-//     const games = await getGamesByName(name);
-
-//     res.status(200).json(games);
-// }));
 
 // router.post('/', errorHandler(async (req, res) => {
 //     const { game } = req.body;
