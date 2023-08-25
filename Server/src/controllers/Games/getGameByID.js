38d14aef95c76923ba6ecb16528dddabb1c46bc6
fernandoiosono <fernandoiosono@gmail.com>
2023-08-25 00:49:07 -0600

    Code Reduced / Sequelize Code Optimization / Now Return Names of Genres & Platforms

--- Server/src/controllers/Games/getGameByID.js
+++ Server/src/controllers/Games/getGameByID.js
@@ -2,77 +2,44 @@ require('dotenv').config();
 const axios = require('axios');
 
 const { Game, 
-    GameGenre, 
-    GamePlatform } = require('../../database/database.js');
+    Genre,
+    Platform } = require('../../database/database.js');
 
 const { RAWG_URL_GAMES, RAWG_API_KEY } = process.env;
 
-const getVGByID = async (pID) => {
+const getGameByID = async (pID) => {
     if (isNaN(pID)) {
-        const dataGame = await Game.findOne({
-            where: { idGame: pID }
-
-            // Checar por qué este código no funciona
-            // Arroja un Error que dice que no hay relación entre las tablas :(
-            // Al parecer con este código nos ahorraríamos la siguientes 2 consultas...
-
-            // include: [
-            //     { model: GamesGenres, attributes: 'idGenre' },
-            //     { model: GamesPlatforms, attributes: 'idPlatform' }
-            // ]
-        });
-
-        const genres = await GameGenre.findAll({
+        const game = await Game.findOne({
             where: { idGame: pID },
-            attributes: ['idGenre']
+            include: [
+                { model: Genre, attributes: ['name'] },
+                { model: Platform, attributes: ['name'] }
+            ]
         });
 
-        const platforms = await GamePlatform.findAll({
-            where: { idGame: pID },
-            attributes: ['idPlatform']
-        });
+        game.dataValues.genres = game.dataValues.Genres.map(genre => genre.dataValues.name);
+        game.dataValues.platforms = game.dataValues.Platforms.map(platform => platform.dataValues.name);
 
-        const { name, image, released, rating,
-            description } = dataGame;
-
-            const game = { id: pID, name, image, released, rating, description,
-                genres: getArrIDs(genres, 'genre', 'database'), 
-                platforms: getArrIDs(platforms, 'platform', 'database') };
+        delete game.dataValues.Genres;
+        delete game.dataValues.Platforms;
 
         return game;
     } else {
         const { data } = await axios(`${RAWG_URL_GAMES}/${pID}?key=${RAWG_API_KEY}`);
 
-        const { id, name, background_image, released, rating,
-            description, platforms, genres } = data;
-
-        const game = { id, name, image: background_image, released, rating, description,
-            genres: getArrIDs(genres, 'genre', 'api'), 
-            platforms: getArrIDs(platforms, 'platform', 'api') };
+        const game = {
+            idGame: data.id,
+            name: data.name,
+            image: data.background_image,
+            released: data.released,
+            rating: data.rating,
+            description: data.description,
+            genres: data.genres.map(genre => genre.name),
+            platforms: data.platforms.map(platform => platform.platform.name)
+        }
 
         return game;
     }
 };
 
-const getArrIDs = (obj, type, origin) => { // Get an Ordered Array of ID Genres
-    const arrIDs = [];
-
-    for (let x = 0; x < obj.length; x++) {
-        const id = 
-            (origin === "database")
-                ? (type === "genre")
-                    ? obj[x].dataValues.idGenre
-                    : obj[x].dataValues.idPlatform
-                : (type === "genre")
-                    ? obj[x].id
-                    : obj[x].platform.id;
-
-        arrIDs.push(id);
-    }
-
-    arrIDs.sort((a, b) => { return a - b });
-
-    return arrIDs;
-};
-
-module.exports = getVGByID;
+module.exports = getGameByID;
