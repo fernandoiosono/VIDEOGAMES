c5363c13cbb03c705c05709718db0a555acfb2c1
fernandoiosono <fernandoiosono@gmail.com>
2023-08-25 09:36:02 -0600

    Basic Structure Complete / Based on Rick & Morty Project

---
+++ Client/src/components/App.jsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import { useSelector } from "react-redux";
+import { Routes, Route } from "react-router-dom";
+// import { Nav, Favorites, Cards, About, Login, CardDetail, PageNotFound } from "../components";
+
+const App = () => {
+	// const userIsAuth = useSelector((state) => state.userIsAuth);
+
+	return (
+		<SectionApp>
+			{/* <header>{userIsAuth ? <Nav /> : null}</header> */}
+			<main>
+				<Routes>
+					{/* <Route path="*" element={<PageNotFound />} />
+					<Route path="/" element={<Login />} />
+					<Route path="/home" element={<Cards />} />
+					<Route path="/favorites" element={<Favorites />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/carddetail/:fatherComp/:id" element={<CardDetail />} /> */}
+				</Routes>
+			</main>
+		</SectionApp>
+	);
+};
+
+const SectionApp = styled.section`
+	text-align: center;
+	padding: 25px;
+`;
+
+export default App;
---
+++ Client/src/main.jsx
@@ -0,0 +1,14 @@
+import "./assets/css/index.css";
+import { App } from "./components";
+// import { store } from "./redux/store.js";
+// import { Provider } from "react-redux";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	// <Provider store={store}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	// </Provider>
+);
