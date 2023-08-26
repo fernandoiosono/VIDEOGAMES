ad67589cd335889c89138b9b77095cad3db2ab4b
fernandoiosono <fernandoiosono@gmail.com>
2023-08-26 01:56:48 -0600

    Advances / Not Finished

--- Client/src/components/App.jsx
+++ Client/src/components/App.jsx
@@ -1,22 +1,25 @@
-import styled from "styled-components";
-import { useSelector } from "react-redux";
-import { Routes, Route } from "react-router-dom";
+// import { useSelector } from "react-redux";
 // import { Nav, Favorites, Cards, About, Login, CardDetail, PageNotFound } from "../components";
 
-const App = () => {
-	// const userIsAuth = useSelector((state) => state.userIsAuth);
+import styled from "styled-components";
+import { Routes, Route } from "react-router-dom";
 
+import { Home, 
+	Landing, 
+	PageNotFound } from "../components";
+
+const App = () => {
 	return (
 		<SectionApp>
 			{/* <header>{userIsAuth ? <Nav /> : null}</header> */}
 			<main>
 				<Routes>
-					{/* <Route path="*" element={<PageNotFound />} />
-					<Route path="/" element={<Login />} />
-					<Route path="/home" element={<Cards />} />
-					<Route path="/favorites" element={<Favorites />} />
-					<Route path="/about" element={<About />} />
-					<Route path="/carddetail/:fatherComp/:id" element={<CardDetail />} /> */}
+					<Route path="*" element={<PageNotFound />} />
+					<Route path="/" element={<Landing />} />
+					<Route path="/home" element={<Home />} />
+					{/* <Route path="/favorites" element={<Favorites />} /> */}
+					{/* <Route path="/about" element={<About />} /> */}
+					{/* <Route path="/carddetail/:fatherComp/:id" element={<CardDetail />} /> */}
 				</Routes>
 			</main>
 		</SectionApp>
