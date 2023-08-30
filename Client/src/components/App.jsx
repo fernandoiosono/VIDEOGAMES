df245bd9e8cc1a2f8909e9f24a1b186d73747177
fernandoiosono <fernandoiosono@gmail.com>
2023-08-30 00:12:12 -0600

    Views Routing Finished

--- Client/src/components/App.jsx
+++ Client/src/components/App.jsx
@@ -1,25 +1,21 @@
-// import { useSelector } from "react-redux";
-// import { Nav, Favorites, Cards, About, Login, CardDetail, PageNotFound } from "../components";
-
 import styled from "styled-components";
 import { Routes, Route } from "react-router-dom";
 
-import { Home, 
-	Landing, 
-	PageNotFound } from "../components";
+import { 
+	NewGame,
+	GameDetail,
+	Home, 
+	Landing } from "../views";
 
 const App = () => {
 	return (
 		<SectionApp>
-			{/* <header>{userIsAuth ? <Nav /> : null}</header> */}
 			<main>
 				<Routes>
-					<Route path="*" element={<PageNotFound />} />
 					<Route path="/" element={<Landing />} />
 					<Route path="/home" element={<Home />} />
-					{/* <Route path="/favorites" element={<Favorites />} /> */}
-					{/* <Route path="/about" element={<About />} /> */}
-					{/* <Route path="/carddetail/:fatherComp/:id" element={<CardDetail />} /> */}
+					<Route path="/newgame" element={<NewGame />} />
+					<Route path="/gamedetail/:id" element={<GameDetail />} />
 				</Routes>
 			</main>
 		</SectionApp>
