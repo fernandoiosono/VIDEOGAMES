81d7d0b1596819284f70ae4b9434ec8e6d19a1ba
fernandoiosono <fernandoiosono@gmail.com>
2023-08-30 00:08:47 -0600

    Landing View Finished / Genres & Platforms Initial Charge Finished

--- Client/src/views/Landing.jsx
+++ Client/src/views/Landing.jsx
@@ -1,14 +1,36 @@
+import { useEffect } from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { setAllGenres, setAllPlatforms } from "../redux/actions.js";
 
 const Landing = () => {
     const navigate = useNavigate();
+    const dispatch = useDispatch();
 
-    return (<>
-        <h1>Welcome To Game Universe!</h1>
-        <ButtonHome onClick={() => navigate("/home")}>Go Home</ButtonHome>
-    </>);
+    useEffect(() => {
+        dispatch(setAllGenres());
+        dispatch(setAllPlatforms());
+    }, []);
+
+    return (
+        <SectionLanding>
+            <H1Welcome>Welcome To Game Universe!</H1Welcome>
+            <ButtonHome onClick={() => navigate("/home")}>Go Home</ButtonHome>
+        </SectionLanding>
+    );
 };
+
+const SectionLanding = styled.section`
+    padding-top: 30%;  
+`;
+
+const H1Welcome = styled.h1`
+    text-shadow: 0 0 10px black;
+    font-size: 40px;
+    color: white;
+    margin-bottom: 15px;
+`;
 
 const ButtonHome = styled.button`
     font-weight: bold;
@@ -16,8 +38,10 @@ const ButtonHome = styled.button`
     border-radius: 5px;
     height: 30px;
     width: 100px;
+    box-shadow: 0 0 10px black;
 
     &:hover {
+        box-shadow: 0 0 10px white;
         background-color: white;
         cursor: pointer;
     }
