43f4d8974ff80fa0287a2a3409de3a9f1e92123e
fernandoiosono <fernandoiosono@gmail.com>
2023-08-31 01:56:50 -0600

    GameDetail View Changed to Detail

---
+++ Client/src/views/Detail.jsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+import { setNavFather } from "../redux/actions.js";
+import { NavigationBar, GameDetail } from "../components";
+
+const Detail = () => {
+    const dispatch = useDispatch();
+    const { id } = useParams();
+
+    alert(id);
+    
+    useEffect(() => {
+        dispatch(setNavFather('detail'));
+        // dispatch(setCharacterDetail(id));
+
+        // return () => { dispatch(cleanDetail()); };
+    }, [id]);
+
+    return (<>
+        <NavigationBar />
+        <GameDetail />
+    </>);
+};
+
+export default Detail;
