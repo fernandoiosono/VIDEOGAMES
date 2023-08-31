026041f77427c52e959d111c3a577afc5b528669
fernandoiosono <fernandoiosono@gmail.com>
2023-08-31 02:03:56 -0600

    Basic Structure Finished

--- Client/src/components/GameCards.jsx
+++ Client/src/components/GameCards.jsx
@@ -1,5 +1,11 @@
-const GameCards = () => {
+import { useSelector } from "react-redux";
 
+const GameCards = () => {
+    const gamesByName = useSelector((state) => state.gamesByName)
+
+    console.log(gamesByName);
+
+    return (<></>);
 };
 
 export default GameCards;
