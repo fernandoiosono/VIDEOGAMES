b037f3f2518a3d5e399f97e4ed248f019f92327c
fernandoiosono <fernandoiosono@gmail.com>
2023-08-31 02:11:28 -0600

    Basic Structure Finished

--- Client/src/views/NewGame.jsx
+++ Client/src/views/NewGame.jsx
@@ -1,5 +1,19 @@
-const NewGame = () => {
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { NavigationBar, NewGameForm } from "../components";
+import { setNavFather } from "../redux/actions.js";
 
+const NewGame = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(setNavFather('newGame'));
+    }, []);
+
+    return (<>
+        <NavigationBar />
+        <NewGameForm />
+    </>);
 };
 
 export default NewGame;
