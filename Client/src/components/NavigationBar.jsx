0498f45af2a1c1f4e09feade48d65a83950c7816
fernandoiosono <fernandoiosono@gmail.com>
2023-08-31 02:00:19 -0600

    Component Name Changed to NavigationBar / Now Displays the Site Name & a ToolBar

---
+++ Client/src/components/NavigationBar.jsx
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { ToolBar } from ".";
+
+const NavigationBar = () => {
+    return (<>
+        <Navigation>
+            <ArticleLinks>
+                <Link to="/home" style={{ textDecoration: 'none', color: 'black' }}>
+                    <ImgIcon src="/icon.svg" alt="mainIcon" />
+                </Link>
+                <Link to="/home" style={{ textDecoration: 'none', color: 'black' }}>
+                    <H1>Game Universe</H1>
+                </Link>
+            </ArticleLinks>
+            <ArticleToolBar>
+                <ToolBar />
+            </ArticleToolBar>
+        </Navigation>
+    </>);
+};
+
+const Navigation = styled.nav`
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    font-weight: bold;
+    /* background: rgba(255, 255, 255, 0.4); */
+`;
+
+const Article = styled.article` display: flex; `;
+
+const ArticleLinks = styled(Article)`
+    width: 40%;
+    justify-content: left;
+`;
+
+const ArticleToolBar = styled(Article)`
+    width: 60%;
+    justify-content: right;
+`;
+
+const ImgIcon = styled.img`
+    display: flex;
+    height: 40px;
+    margin-right: 10px;
+    margin-bottom: 0px;
+`;
+
+const H1 = styled.h1`
+    margin-top: 7px;
+    margin-bottom: 0px;
+    height: 25px;
+    font-size: 33px;
+    display: flex;
+    align-items: center;
+    color: white;
+    text-shadow: 0 0 10px black;
+`;
+
+export default NavigationBar;
