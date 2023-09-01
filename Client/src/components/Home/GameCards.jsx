6a109da0498c6e5a9fe64139bb3268bc09ccd362
fernandoiosono <fernandoiosono@gmail.com>
2023-09-01 16:04:30 -0600

    Improvement in the Structuring of Components Through Module Folders

--- Client/src/components/Home/GameCards.jsx
+++ Client/src/components/Home/GameCards.jsx
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 const GameCards = () => {
     const gamesByName = useSelector((state) => state.gamesByName)
 
-    console.log(gamesByName);
+    // console.log(gamesByName);
 
     return (<></>);
 };
--- Client/src/components/index.js
+++ Client/src/components/index.js
@@ -4,13 +4,18 @@ import NewGameButton from "./NavigationBar/NewGameButton.jsx";
 import SearchBar from "./NavigationBar/SearchBar.jsx";
 import ToolBar from "./NavigationBar/ToolBar.jsx";
 
+// Detail View
+import GameDetail from "./Detail/GameDetail.jsx";
+
+// Home View
+import GameCards from "./Home/GameCards.jsx";
+
 // NewGame View
 import NewGameForm from "./NewGame/NewGameForm.jsx";
 
 // Global
 import App from "./App.jsx";
-import GameCards from "./GameCards.jsx";
-import GameDetail from "./GameDetail.jsx";
+
 import NavigationBar from "./NavigationBar.jsx";
 
 export {
