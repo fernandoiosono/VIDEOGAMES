5772d756e093e2bbcde8e268d04a21dfc1769fd2
fernandoiosono <fernandoiosono@gmail.com>
2023-09-08 11:22:05 -0600

    SET_ALL_GAMES Added

--- Client/src/redux/actionTypes.js
+++ Client/src/redux/actionTypes.js
@@ -1,6 +1,7 @@
 export const GET_ALL_GAMES = "GET_ALL_GAMES";
 export const GET_GAMES_BY_NAME = "GET_GAMES_BY_NAME";
 
+export const SET_ALL_GAMES = "SET_ALL_GAMES";
 export const SET_NAV_FATHER = "SET_NAV_FATHER";
 export const SET_ALL_GENRES = "SET_ALL_GENRES";
 export const SET_GAME_DETAIL = "SET_GAME_DETAIL";
