d96230618ba82a82327eef0547beee9659d8e248
fernandoiosono <fernandoiosono@gmail.com>
2023-09-09 08:13:59 -0600

    IF Structure Changed to Switch to Avoid Unnecessary Validations if Something is Wrong

--- Client/src/helpers/NewGame/isValidURL.js
+++ Client/src/helpers/NewGame/isValidURL.js
@@ -3,10 +3,15 @@ const isURL = (str) => {
 
     const isUrl = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
     const isImage = /\.(jpg|jpeg|png|svg)$/i;
-
-    if (!isUrl.test(str)) error = "The Clipboard's Content Is Not an URL!\n\nCurrent Clipboard Content: \n" + str;
-    if (!isImage.test(str)) error = "The URL Must Correspond to an Image!\nAllowed Formats: JPG, JPEG, PNG, SVG\n\nCurrent Clipboard Content: \n" + str;
     
+    switch (true) {
+        case !isUrl.test(str):
+            error = "The Clipboard's Content Is Not an URL!\n\nCurrent Clipboard Content: \n" + str;
+            break;
+        case !isImage.test(str):
+            error = "The URL Must Correspond to an Image!\nAllowed Formats: JPG, JPEG, PNG, SVG\n\nCurrent Clipboard Content: \n" + str;
+    }
+
     // Falta evaluar si la URL existe
     // Checar Rick&Morty > LoginForm > handleSubmit
 
