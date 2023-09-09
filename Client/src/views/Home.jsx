d4dca4b407837c83c73f121979d46f78101e998c
fernandoiosono <fernandoiosono@gmail.com>
2023-09-09 08:15:21 -0600

    Now Charge All the Games Object on Component Mount

--- Client/src/views/Home.jsx
+++ Client/src/views/Home.jsx
@@ -1,13 +1,15 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { setNavFather } from "../redux/actions.js";
+import { useDispatch, useSelector } from "react-redux";
+import { setAllGames, setNavFather } from "../redux/actions.js";
 import { NavigationBar, GameCards } from "../components";
 
 const Home = () => {
     const dispatch = useDispatch();
-    
+    const allGames = useSelector((state) => state.allGames);
+
     useEffect(() => {
         dispatch(setNavFather('home'));
+        dispatch(setAllGames());
     }, []);
 
     return (<>
