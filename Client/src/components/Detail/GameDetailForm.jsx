95777f7d58de49af290ea814489c4811075ec4b5
fernandoiosono <fernandoiosono@gmail.com>
2023-09-09 14:34:07 -0600

    Functionality Completed / Release Date Display is Missing

--- Client/src/components/Detail/GameDetailForm.jsx
+++ Client/src/components/Detail/GameDetailForm.jsx
@@ -2,99 +2,19 @@ import styled from "styled-components";
 import { useState, useEffect } from "react";
 import star from "../../assets/img/star.svg";
 import unstar from "../../assets/img/unstar.svg";
-import { addNewGame, setAllGames } from "../../redux/actions.js";
 import { useSelector, useDispatch } from "react-redux";
 import { setAllGenres, setAllPlatforms } from "../../redux/actions.js";
-import { errorNewGameForm, isValidURL, validateNewGameData } from "../../helpers";
 
 const GameDetailForm = () => {
     const dispatch = useDispatch();
 
     const allGenres = useSelector((state) => state.allGenres);
     const allPlatforms = useSelector((state) => state.allPlatforms);
+    const gameData = useSelector((state) => state.gameDetail)
 
-    const initGameData = { 
-        name: "", 
-        description: "", 
-        image: "", 
-        rating: 0, 
-        genres: [], 
-        platforms: [] };
-
-    const [errors, setErrors] = useState({ 
-        name: "", 
-        description: "", 
-        image: "", 
-        rating: "", 
-        genres: "", 
-        platforms: "" });
-
-    const [gameData, setGameData] = useState(initGameData);
-
-    const handleInputChange = (e) => { // Name, Description, Image URL
-		const property = e.target.name,
-			value = e.target.value;
-
-		// De ésta forma usamo una copia actualizada del estado
-		const gameDataUpdated = { ...gameData, [property]: value };
-
-		setGameData(gameDataUpdated);
-		validateNewGameData(gameDataUpdated, property, setErrors, errors);
-	};
-
-    const handleRatingChange = (rating) => {
-        const gameDataUpdated = { ...gameData, rating: rating };
-
-        setGameData(gameDataUpdated);
-        validateNewGameData(gameDataUpdated, "rating", setErrors, errors);
+    const handleCheckKeyDown = (e) => {
+        if (e.key === " ") e.preventDefault();
     };
-
-    const handleCheckChange = (e) => {
-        let gameDataUpdated = {};
-
-        const property = e.target.id.split('-')[0];
-        const value = parseInt(e.target.value, 10);
-
-        if (e.target.checked) {
-            gameDataUpdated = { ...gameData, [property]: [ ...gameData[property], value ] };
-        } else {
-            gameDataUpdated = { ...gameData, [property]: [...gameData[property].filter((id) => id !== value)] };
-        }
-
-        setGameData(gameDataUpdated);
-        validateNewGameData(gameDataUpdated, property, setErrors, errors);
-    };
-
-    const handlePasteClipboard = () => {
-        navigator.clipboard.readText().then((pastedText) => {
-            if (isValidURL(pastedText)) {
-                const gameDataUpdated = { ...gameData, image: pastedText }
-
-                setGameData(gameDataUpdated);
-                validateNewGameData(gameDataUpdated, "image", setErrors, errors);
-            }
-        });
-    };
-
-    const handleSubmit = (e) => {
-		e.preventDefault();
-
-		const errorForm = errorNewGameForm(gameData, errors);
-
-		if (!errorForm) {
-            dispatch(addNewGame(gameData))
-                .then(() => {
-                    dispatch(setAllGames());
-
-                    setGameData(initGameData);
-                })
-                .catch((error) => {
-                    
-                });
-		} else {
-			alert(errorForm);
-		}
-	};
 
     useEffect(() => {
         // Recargamos Géneros y Plataformas si está vacío el estado global
@@ -103,24 +23,20 @@ const GameDetailForm = () => {
     }, []);
 
     return (
-        <FormNewGame onSubmit={handleSubmit}>
+        <FormNewGame>
             <MainAside>
                 <TitleGroup>Name</TitleGroup>
-                <TextAName name="name" value={gameData.name} onChange={handleInputChange} />
-                <PError>{errors.name}</PError>
+                <TextAName readOnly name="name" defaultValue={gameData.name} autoComplete="off" />
                 <TitleGroup>Description</TitleGroup>
-                <TextADescription name="description" value={gameData.description} onChange={handleInputChange} />
-                <PError>{errors.description}</PError>
+                <TextADescription readOnly name="description" defaultValue={gameData.description} autoComplete="off" />
                 <TitleGroup>Rating</TitleGroup>
                 <DivRating>
-                    <ImgRating src={(gameData.rating >= 1) ? star : unstar} alt="rating1" onClick={() => handleRatingChange(1)} />
-                    <ImgRating src={(gameData.rating >= 2) ? star : unstar} alt="rating2" onClick={() => handleRatingChange(2)} />
-                    <ImgRating src={(gameData.rating >= 3) ? star : unstar} alt="rating3" onClick={() => handleRatingChange(3)} />
-                    <ImgRating src={(gameData.rating >= 4) ? star : unstar} alt="rating4" onClick={() => handleRatingChange(4)} />
-                    <ImgRating src={(gameData.rating >= 5) ? star : unstar} alt="rating5" onClick={() => handleRatingChange(5)} />
+                    <ImgRating src={(gameData.rating >= 1) ? star : unstar} alt="rating1" />
+                    <ImgRating src={(gameData.rating >= 2) ? star : unstar} alt="rating2" />
+                    <ImgRating src={(gameData.rating >= 3) ? star : unstar} alt="rating3" />
+                    <ImgRating src={(gameData.rating >= 4) ? star : unstar} alt="rating4" />
+                    <ImgRating src={(gameData.rating >= 5) ? star : unstar} alt="rating5" />
                 </DivRating>
-                <PError>{errors.rating}</PError>
-                <ButtonSave type="submit">Save</ButtonSave>
             </MainAside>
             <AsideLeft>
                 <ArticleImg>
@@ -128,45 +44,41 @@ const GameDetailForm = () => {
                     <DivImage>
                         <ImgGame src={gameData.image} />
                     </DivImage>
-                    <FooterImage>
-                        <TextAURL type="text" name="image" readOnly value={gameData.image} onChange={handleInputChange} />
-                        <ButtonPaste type="button" onClick={handlePasteClipboard}>Paste</ButtonPaste>
-                    </FooterImage>
-                    <PError>{errors.image}</PError>
+                    <TextAURL readOnly name="image" defaultValue={gameData.image} autoComplete="off"/>
                 </ArticleImg>
             </AsideLeft>
             <AsideRight>
                 <ArticleGen>
                     <TitleGroup>Genres</TitleGroup>
                     <DivScroll>
-                        {allGenres.map(({idGenre, name, slug}) => (<>
-                            <input type="checkbox" 
-                                key={slug} 
-                                id={`genres-${slug}`} // Don´t Change This Structure (handleCheckChange > type Variable)
-                                value={idGenre}
-                                checked={gameData.genres.includes(idGenre)}
-                                onChange={handleCheckChange} />
-                            <label htmlFor={`genre-${slug}`}>{name}</label>
-                            <br />
-                        </>))}
+                        {allGenres.map(({idGenre, name, slug}) => (
+                            <div key={`chkgenre-${slug}`}>
+                                <CheckBox readOnly
+                                    type="checkbox" 
+                                    id={`genres-${slug}`}
+                                    defaultChecked={gameData.genres && gameData.genres.includes(idGenre)}
+                                    onKeyDown={handleCheckKeyDown} />
+                                <LabelCheck htmlFor={`genres-${slug}`}>{name}</LabelCheck>
+                                <br />
+                            </div>
+                        ))}
                     </DivScroll>
-                    <PError>{errors.genres}</PError>
                 </ArticleGen>
                 <ArticleGen>
                     <TitleGroup>Platforms</TitleGroup>
                     <DivScroll>    
-                        {allPlatforms.map(({idPlatform, name, slug}) => (<>
-                            <input type="checkbox" 
-                                key={slug} 
-                                id={`platforms-${slug}`} // Don´t Change This Structure (handleCheckChange > type Variable)
-                                value={idPlatform}
-                                checked={gameData.platforms.includes(idPlatform)}
-                                onChange={handleCheckChange} />
-                            <label htmlFor={`platform-${slug}`}>{name}</label>
+                        {allPlatforms.map(({idPlatform, name, slug}) => (
+                            <div key={`chkplatform-${slug}`}>
+                            <CheckBox readOnly
+                                type="checkbox" 
+                                id={`platforms-${slug}`}
+                                defaultChecked={gameData.platforms && gameData.platforms.includes(idPlatform)}
+                                onKeyDown={handleCheckKeyDown} />
+                            <LabelCheck htmlFor={`platforms-${slug}`}>{name}</LabelCheck>
                             <br />
-                        </>))}
+                        </div>
+                        ))}
                     </DivScroll>
-                    <PError>{errors.platforms}</PError>
                 </ArticleGen>
             </AsideRight>
         </FormNewGame>
@@ -189,53 +101,8 @@ const MainAside = styled.main`
     background-color: rgba(0, 0, 0, 0.3);
 
     display: grid;
-    grid-template-rows: auto auto auto auto 1fr auto auto auto auto auto;
+    grid-template-rows: auto auto auto 1fr auto auto;
     grid-gap: 10px;
-`;
-
-const ButtonSave = styled.button`
-	height: 35px;
-	width: 100%;
-	border: none;
-	border-radius: 5px;
-	background-color: green;
-	color: white;
-	font-weight: bold;
-	font-size: 11px;
-	box-shadow: 0 0 5px black;
-	text-shadow: 0 0 15px white;
-    margin: 0 auto;
-
-	&:hover { 
-		cursor: pointer;
-		box-shadow: 0 0 5px white;
-	}
-`;
-
-const PError = styled.p`
-	font-size: 10px;
-	background-color: #c52727;
-	color: white;
-	margin: 0;
-    border-radius: 2px;
-    text-align: center;
-`;
-
-const ButtonPaste = styled.button`
-    width: 60px;
-	border: none;
-	border-radius: 5px;
-	background-color: #4d6bda;
-	color: white;
-	font-weight: bold;
-	font-size: 10px;
-	box-shadow: 0 0 5px black;
-	text-shadow: 0 0 15px white;
-
-	&:hover { 
-		cursor: pointer;
-		box-shadow: 0 0 5px white;
-	}
 `;
 
 const Aside = styled.aside`
@@ -264,11 +131,11 @@ const Article = styled.article`
 `;
 
 const ArticleImg = styled(Article)`
-    grid-template-rows: auto 1fr auto auto;
+    grid-template-rows: auto 1fr auto;
 `;
 
 const ArticleGen = styled(Article)`
-    grid-template-rows: auto 1fr auto;
+    grid-template-rows: auto 1fr;
 `;
 
 const Paragraph = styled.p`
@@ -280,12 +147,6 @@ const Paragraph = styled.p`
 `;
 
 const TitleGroup = styled(Paragraph)``;
-
-const FooterImage = styled.footer`    
-    display: grid;
-    grid-template-columns: 1fr auto;
-    grid-gap: 10px;
-`;
 
 const DivRating = styled.div`
     padding: 20px;
@@ -322,7 +183,6 @@ const DivScroll = styled.div`
 const ImgRating = styled.img`
     height: 40px;
     filter: drop-shadow(-2px 2px 2px rgba(0, 0, 0, 0.7));
-    &:hover { cursor: pointer; }  
 `;
 
 const ImgGame = styled.img`
@@ -367,6 +227,16 @@ const TextADescription = styled.textarea`
     background-color: rgba(255, 255, 255, 0.7);
 
     &:focus { outline: none; }
+`;
+
+const CheckBox = styled.input`
+    pointer-events: none;
+
+    &:focus { outline: none; }
+`;
+
+const LabelCheck = styled.label`
+    pointer-events: none;
 `;
 
 export default GameDetailForm;
