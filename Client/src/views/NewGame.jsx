e771b4d57763076a33e7a35ac1657191690dd5d6
fernandoiosono <fernandoiosono@gmail.com>
2023-09-09 09:25:25 -0600

    Minor Importation Change

--- Client/src/views/NewGame.jsx
+++ Client/src/views/NewGame.jsx
@@ -1,8 +1,8 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { NavigationBar, NewGameForm } from "../components";
-import { setNavFather } from "../redux/actions.js";
 import { styled } from "styled-components";
+import { setNavFather } from "../redux/actions.js";
+import { NavigationBar, NewGameForm } from "../components";
 
 const NewGame = () => {
     const dispatch = useDispatch();
