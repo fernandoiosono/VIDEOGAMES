cd7e60c5697b600b420b69a902cda40769267b5b
fernandoiosono <fernandoiosono@gmail.com>
2023-09-12 19:26:46 -0600

    It Doesn´t Charge Games Anymore

--- Client/src/views/Landing.jsx
+++ Client/src/views/Landing.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import styled from "styled-components";
-import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { setAllGenres, setAllPlatforms } from "../redux/actions.js";
 
 const Landing = () => {
